407b56f22a4d8e982919d7fb7c755022b9a4cf3a
Lalit Bohara <lalitbohara6395@gmail.com>
2026-04-17 20:32:22 +0530

    second laptop se

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,5 +5,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
 // Supabase client create karo
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
